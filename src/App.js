d749e5c56d780b6e914f4f7fd5f2ca44c29fe1f4
shipwoli <khalwalesaviour@gmail.com>
2024-08-09 14:03:59 +0300

    finalizing header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from "./Components/Header/Header";
 import HomePage from "./Components/Homepage/HomePage";
 import Footer from "./Components/Foooter/Footer";
+// index.js or App.js
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 
 function App() {
   return (
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,38 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import './Header.css'; // Add this line if you're using a separate CSS file
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Header.css';  // Custom CSS for additional styling
+import logo from './stem.png';  // Import the logo
+import { FaSearch } from 'react-icons/fa';  // Import FontAwesome for the search icon
+import kenyaFlag from './kenya.png';  // Import Kenya flag image
 
-function Header() {
+const Header = () => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
   return (
-    <header>
-      <nav className="navbar">
-        <div className="navbar-logo">
-          <NavLink to="/">Stemtrix</NavLink>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          <img src={logo} alt="Logo" height="40" />
+        </a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <NavLink exact to="/" className="nav-link" activeClassName="active">Home</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/about" className="nav-link" activeClassName="active">About Us</NavLink>
+            </li>
+            <li className="nav-item dropdown">
+              <NavLink to="/services" className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Services</NavLink>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><NavLink to="/services/service1" className="dropdown-item">Service 1</NavLink></li>
+                <li><NavLink to="/services/service2" className="dropdown-item">Service 2</NavLink></li>
+                <li><NavLink to="/services/service3" className="dropdown-item">Service 3</NavLink></li>
+              </ul>
+            </li>
+            <li className="nav-item dropdown">
+              <NavLink to="/products" className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Products</NavLink>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><NavLink to="/products/product1" className="dropdown-item">Product 1</NavLink></li>
+                <li><NavLink to="/products/product2" className="dropdown-item">Product 2</NavLink></li>
+                <li><NavLink to="/products/product3" className="dropdown-item">Product 3</NavLink></li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/contact" className="nav-link" activeClassName="active">Contacts</NavLink>
+            </li>
+            {/* Search Icon */}
+            <li className="nav-item">
+              <NavLink to="/search" className="nav-link"><FaSearch /></NavLink>
+            </li>
+            {/* Flag Selector */}
+            <li className="nav-item dropdown">
+              <div className="nav-link flag-selector" onClick={toggleDropdown}>
+                <img src={kenyaFlag} alt="Kenya Flag" height="25" className="rounded-circle" />
+              </div>
+              {dropdownOpen && (
+                <ul className="dropdown-menu show flag-dropdown">
+                  <li className="dropdown-item">Kenya</li>
+                  <li className="dropdown-item">International</li>
+                </ul>
+              )}
+            </li>
+          </ul>
         </div>
-        <ul className="navbar-links">
-          <li>
-            <NavLink exact to="/" activeClassName="active-link">
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/about-us" activeClassName="active-link">
-              About Us
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/classes" activeClassName="active-link">
-              Classes
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact" activeClassName="active-link">
-              Contact
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
+      </div>
+    </nav>
   );
 }
 
