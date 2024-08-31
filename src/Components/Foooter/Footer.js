5d2960f34248ce76f6362feedc1ff7286eac6bea
shipwoli <khalwalesaviour@gmail.com>
2024-08-31 13:21:44 +0300

    styling the navbar

--- src/Components/Foooter/Footer.js
+++ src/Components/Foooter/Footer.js
@@ -5,24 +5,24 @@ import './Footer.css';
 
 const Footer = () => {
   return (
-    <footer className="footer bg-blue text-black py-3">
+    <footer className="footer">
       <Container>
         <Row className="align-items-center">
-          <Col md={6}>
+          <Col md={6} className="footer-left">
             <p>© 2024 Stemtrix. All rights reserved.</p>
           </Col>
-          <Col md={6} className="text-md-right">
+          <Col md={6} className="footer-right text-md-right">
             <p className="mb-0">Follow us on:</p>
-            <a href="https://www.instagram.com/stemtrix2/" target="_blank" rel="noopener noreferrer" className="ml-2">
+            <a href="https://www.instagram.com/stemtrix2/" target="_blank" rel="noopener noreferrer" className="social-icon">
               <FaInstagram />
             </a>
-            <a href="https://web.facebook.com/search/top?q=stemtrix%20ea" target="_blank" rel="noopener noreferrer" className="ml-2">
+            <a href="https://web.facebook.com/search/top?q=stemtrix%20ea" target="_blank" rel="noopener noreferrer" className="social-icon">
               <FaFacebookF />
             </a>
-            <a href="https://www.linkedin.com/in/stemtrix-ea-5ab056204/">
+            <a href="https://www.linkedin.com/in/stemtrix-ea-5ab056204/" className="social-icon">
               <FaLinkedinIn />
             </a>
-            <a href="https://x.com/stemtrix" target="_blank" rel="noopener noreferrer" className="ml-2">
+            <a href="https://x.com/stemtrix" target="_blank" rel="noopener noreferrer" className="social-icon">
               <FaTwitter />
             </a>
           </Col>
